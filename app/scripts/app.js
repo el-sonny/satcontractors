15eb5d0a811c07f6d3bef6366d99c7a0aec56bfc
El Sonny <sonny@spaceshiplabs.com>
2016-12-02 11:27:29 -0500

    'prototype'

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,19 +15,14 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngMaterial'
   ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+        controllerAs: 'vm'
       })
       .otherwise({
         redirectTo: '/'
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,19 @@
  * Controller of the satcontractorsApp
  */
 angular.module('satcontractorsApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function($http) {
+    var vm = this;
+
+    vm.init = init;
+
+    vm.init();
+
+    function init() {
+      $http.get('https://raw.githubusercontent.com/spaceship-labs/listas-negras-sat/master/json/intersection.json')
+        .then(function(result) {
+          vm.companies = result.data;
+        });
+    }
+
+
   });
