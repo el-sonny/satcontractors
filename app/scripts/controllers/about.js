ae89407ef047712325f1c3278555897d6366fd43
El Sonny <sonny@spaceshiplabs.com>
2016-12-01 22:18:15 -0500

    yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name satcontractorsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the satcontractorsApp
+ */
+angular.module('satcontractorsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name satcontractorsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the satcontractorsApp
+ */
+angular.module('satcontractorsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
