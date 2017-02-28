195f5d51c4b76b40d5627dde83eaf02ab315019a
El Sonny <sonny@spaceshiplabs.com>
2017-02-28 11:00:49 -0500

    lista definitivos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,12 +11,22 @@ angular.module('satcontractorsApp')
   .controller('MainCtrl', function($http) {
     var vm = this;
 
+    vm.changeList = changeList;
     vm.init = init;
+    vm.list = 'presuntos';
 
     vm.init();
 
+    function changeList(list) {
+      vm.list = list;
+      vm.init();
+    }
+
     function init() {
-      $http.get('https://raw.githubusercontent.com/spaceship-labs/listas-negras-sat/master/json/intersection.json')
+      var url = 'https://raw.githubusercontent.com/spaceship-labs/listas-negras-sat/master/json/' +
+        vm.list +
+        '-intersection.json';
+      $http.get(url)
         .then(function(result) {
           vm.companies = result.data;
         });
